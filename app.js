ac106e6288205b18c0c915455a688c554f2a46f0
Shanakan78 <shanakan.p78@gmail.com>
2025-07-21 17:54:04 +0700

    add all file of project ep4
    Hello

---
+++ app.js
@@ -0,0 +1,28 @@
+// This file for run main service
+
+import express from 'express';  // import file express
+//chalk use for check response eazy by fig color at value
+import chalk from 'chalk';  // import chalk after npm install chalk
+import debug from 'debug';
+import morgan from 'morgan'
+
+const app = express() // use express as app
+const port = 3000 
+
+app.use(morgan('combined'));
+
+// original javascript
+app.get("/",(req,res) => {
+
+    res.send('boen to dev');
+})
+
+
+// test port
+app.listen(port, ()=> {
+    // console.log(`Listening on port ${port}`);
+    // console.log(`Listening on port ${port}`+ chalk.red("xx"));
+    debug(`Listening on port ${port}`+ chalk.red("xx"));
+})
+
+// run by => node app.js
