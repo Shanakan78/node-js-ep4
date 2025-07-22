9a698e4717cb94cf86d2deea650544df01b66c84
Shanakan78 <shanakan.p78@gmail.com>
2025-07-22 18:38:06 +0700

    Update .gitignore

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 
 import express from 'express';  // import file express
 //chalk use for check response eazy by fig color at value
+
 import chalk from 'chalk';  // import chalk after npm install chalk
 import debug from 'debug';
-import morgan from 'morgan'
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express() // use express as app
 const port = 3000 
 
 app.use(morgan('combined'));
-
+app.use(express.static(path.join(__dirname, 'public'))); // call public path for working if find index.html file in public folder
 // original javascript
 app.get("/",(req,res) => {
 
